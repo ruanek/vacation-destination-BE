13a1dbd6dd108f8125c0b7a884a0f61964af6a20
Kyle Ruane <108101096+ruanek@users.noreply.github.com>
2022-07-20 15:01:58 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import express from "express";
+import cors from "cors";
+const app = express();
+
+app.use(cors());
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port:${PORT}`);
+});
+
+const URL = "https://api.kanye.rest";
+app.get("/quotes", async (req, res) => {
+  try {
+    const response = await axios(URL);
+    res.send(response.data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello Kyle");
+});
