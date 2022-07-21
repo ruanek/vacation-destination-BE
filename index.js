e4627bb7c437b1412bf5aef02acfbec155b06d89
Kyle Ruane <108101096+ruanek@users.noreply.github.com>
2022-07-21 09:33:33 -0700

    updating with comments

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+//importing module from express
 import express from "express";
+//importing module from cors
 import cors from "cors";
+//setting the express function to app
 const app = express();
-
+// telling the api to use cors() as middleware
 app.use(cors());
-
+//allows heroku to port select
 const PORT = process.env.PORT || 3000;
+//allows a "deaf" api to listen to the selected port and logs that it is running
 app.listen(PORT, () => {
   console.log(`Server listening on port:${PORT}`);
 });
-
+//Stand in Database
 const destinationsDB = {
   123456: {
     destination: "Eiffel Tower",
@@ -23,16 +27,13 @@ const destinationsDB = {
       "https://images.unsplash.com/photo-1529655683826-aba9b3e77383?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80",
   },
 };
-
+//Read(GET) request to respond witht he obj of the db
 app.get("/destination", (req, res) => {
-  try {
-    const response = destinationsDB;
-    res.send(response);
-  } catch (error) {
-    console.log(error);
-  }
+  res.send(destinationsDB);
 });
 
-app.get("/", (req, res) => {
-  res.send("Add the right endpoint dude /destination");
-});
+//Create(POST)
+
+//Update(PUT)
+
+//Delete(DELETE)
