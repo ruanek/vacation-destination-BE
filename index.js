97137f707331b6e4c4f5f1d64decbe64cca69a06
Kyle Ruane <108101096+ruanek@users.noreply.github.com>
2022-07-20 21:45:30 -0700

    added port scanning

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const app = express();
 
 app.use(cors());
 
-// const PORT = process.env.PORT || 3000;
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port:${PORT}`);
 });
